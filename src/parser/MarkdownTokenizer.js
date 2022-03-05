aada733faccd6f00558cba7066448f9de61798a4
Ozelot Vanilla <shuihenyun@qq.com>
2022-03-05 22:44:48 +0800

    decideLineType done at basic level

--- src/parser/MarkdownTokenizer.js
+++ src/parser/MarkdownTokenizer.js
@@ -2,31 +2,95 @@ define(["require", "exports", "../util/FuncLib"], function (require, exports, Fu
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.MarkdownTokenizer = void 0;
-    var SpecialStartings;
-    (function (SpecialStartings) {
-        SpecialStartings[SpecialStartings["*"] = 0] = "*";
-        SpecialStartings[SpecialStartings["#"] = 1] = "#";
-        SpecialStartings[SpecialStartings[">"] = 2] = ">";
-        SpecialStartings[SpecialStartings["-"] = 3] = "-";
-    })(SpecialStartings || (SpecialStartings = {}));
+    var TokenType;
+    (function (TokenType) {
+        /**
+         *
+         */
+        TokenType[TokenType["plaintext"] = 0] = "plaintext";
+        /**
+         *
+         */
+        TokenType[TokenType["line_special_starting"] = 1] = "line_special_starting";
+        /**
+         *
+         */
+        TokenType[TokenType["HTML_tags"] = 2] = "HTML_tags";
+        /**
+         * Like "$$" or "```"
+         */
+        TokenType[TokenType["paired_mul_line_fmt_notation"] = 3] = "paired_mul_line_fmt_notation";
+    })(TokenType || (TokenType = {}));
+    var LineType;
+    (function (LineType) {
+        /**
+         * A line with text or inline-formatted
+         */
+        LineType[LineType["text"] = 0] = "text";
+        /**
+         * Like "====" to show the previous line will be a title, "$$" to show mul-lines formula
+         */
+        LineType[LineType["notation"] = 1] = "notation";
+        /**
+         * Effectively empty lines. Contains only space, tab, or nothing.
+         */
+        LineType[LineType["blank"] = 2] = "blank";
+    })(LineType || (LineType = {}));
     /**
      *
      */
     class MarkdownTokenizer {
         constructor(config) {
-            var _a, _b;
-            if ((_a = config.file_path) !== null && _a !== void 0 ? _a : false) {
+            /**
+             * If the line only has these character's repeating, its type will become `LineType.notation`.
+             */
+            this.line_notation_repeatings = ['-', '='];
+            /**
+            * If the line only contains one of them, its type will become `LineType.notation`.
+            */
+            this.line_notation_fixed_len = ["$$", "```"];
+            this.text_special_startings = ['*', '#', '>', '-', '+'];
+            if (config.file_path ?? false) {
                 this.file_path = config.file_path;
             }
-            else if ((_b = config.original_text) !== null && _b !== void 0 ? _b : false) {
+            else if (config.original_text ?? false) {
                 this.parse(config.original_text);
             }
-        }
-        parse(text) {
         }
         result() {
             console.log("Showing parsed result.");
             return (0, FuncLib_1.readFileSync)(this.file_path);
+        }
+        parse(text) {
+            // The parser will read file line by line.
+            // First, split the origin str by "\n". Multiple "\n" will have a empty string.
+            let lines = text.split(/\n/); // Example: "test\n\nstr" will be ["test", "", "str"].
+            let line_index = 0;
+            let previous_line_type = null;
+            let current_line_type = null;
+            for (let line of lines) {
+                current_line_type = this.decideLineType(line);
+                console.log("LineType: " + LineType[current_line_type] + ",\ttext: " + line);
+                // Search for inline tokens.
+                if (current_line_type == LineType.text) {
+                }
+            }
+        }
+        decideLineType(line_text) {
+            // If the line_text is actually with characters that are thought to be empty.
+            if (line_text.trim().length == 0) {
+                return LineType.blank;
+            }
+            // Check if has fixed-length character, or filled with repeatings.
+            // Trim end to avoid parse error.
+            line_text = line_text.trimEnd();
+            if (this.line_notation_fixed_len.includes(line_text)
+                || (line_text.split("").every(c => c == line_text[0]) // If only repeat one char
+                    && this.line_notation_repeatings.includes(line_text[0]))) {
+                return LineType.notation;
+            }
+            // If not other types of line.
+            return LineType.text;
         }
     }
     exports.MarkdownTokenizer = MarkdownTokenizer;
--- src/parser/MarkdownTokenizer.ts
+++ src/parser/MarkdownTokenizer.ts
@@ -1,14 +1,53 @@
 import { readFileSync } from "../util/FuncLib";
 
-enum SpecialStartings
-{
-    "*", "#", ">", "-"
-}
-
 interface MarkdownTokenizerConfig
 {
     file_path?: string;
     original_text?: string;
+}
+
+interface Token
+{
+    type: TokenType,
+    indent: number,
+    inside: Token[],
+    original_text?: string
+}
+
+enum TokenType
+{
+    /**
+     * 
+     */
+    plaintext,
+    /**
+     * 
+     */
+    line_special_starting,
+    /** 
+     * 
+     */
+    HTML_tags,
+    /**
+     * Like "$$" or "```"
+     */
+    paired_mul_line_fmt_notation,
+}
+
+enum LineType
+{
+    /**
+     * A line with text or inline-formatted
+     */
+    text,
+    /**
+     * Like "====" to show the previous line will be a title, "$$" to show mul-lines formula
+     */
+    notation,
+    /**
+     * Effectively empty lines. Contains only space, tab, or nothing.
+     */
+    blank
 }
 
 /**
@@ -18,11 +57,20 @@ export class MarkdownTokenizer
 {
     readonly file_path?: string;
 
-    private buffer_of_parsed;
+    private buffer_of_parsed: Token[];
 
-    private previous_line_type;
+    /**
+     * If the line only has these character's repeating, its type will become `LineType.notation`.
+     */
+    private line_notation_repeatings = ['-', '='];
 
-    private current_line_type;
+    /**
+    * If the line only contains one of them, its type will become `LineType.notation`.
+    */
+    private line_notation_fixed_len = ["$$", "```"];
+
+    private text_special_startings = ['*', '#', '>', '-', '+'];
+
 
     constructor(config: MarkdownTokenizerConfig)
     {
@@ -30,14 +78,48 @@ export class MarkdownTokenizer
         else if (config.original_text ?? false) { this.parse(config.original_text); }
     }
 
-    private parse(text: string)
-    {
-
-    }
-
     public result(): string
     {
         console.log("Showing parsed result.");
         return readFileSync(this.file_path);
+    }
+
+    private parse(text: string)
+    {
+        // The parser will read file line by line.
+        // First, split the origin str by "\n". Multiple "\n" will have a empty string.
+        let lines: string[] = text.split(/\n/);    // Example: "test\n\nstr" will be ["test", "", "str"].
+        let line_index: number = 0;
+        let previous_line_type: LineType = null;
+        let current_line_type: LineType = null;
+
+        for (let line of lines)
+        {
+            current_line_type = this.decideLineType(line);
+
+            // Search for inline tokens.
+            if (current_line_type == LineType.text)
+            {
+
+            }
+        }
+    }
+
+    private decideLineType(line_text: string): LineType
+    {
+        // If the line_text is actually with characters that are thought to be empty.
+        if (line_text.trim().length == 0) { return LineType.blank; }
+
+        // Check if has fixed-length character, or filled with repeatings.
+        // Trim end to avoid parse error.
+        line_text = line_text.trimEnd();
+        if (
+            this.line_notation_fixed_len.includes(line_text)
+            || (line_text.split("").every(c => c == line_text[0]) // If only repeat one char
+                && this.line_notation_repeatings.includes(line_text[0]))
+        ) { return LineType.notation; }
+
+        // If not other types of line.
+        return LineType.text;
     }
 }
